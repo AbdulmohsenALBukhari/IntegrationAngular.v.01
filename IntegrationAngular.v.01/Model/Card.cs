d5b684de171d62a55c977fafc86bb057b18c0560
nitrous ls <nitrous.1320@outlook.com>
2023-07-30 15:21:54 +0300

    Add project files.

---
+++ IntegrationAngular.v.01/Model/Card.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IntegrationAngular.v._01.Model
+{
+    public class Card
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+    }
+}
